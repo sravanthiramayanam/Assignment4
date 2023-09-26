2673a5064e1dc9e9383488719a592dbf53624a86
sravanthiramayanam <114696618+sravanthiramayanam@users.noreply.github.com>
2023-09-25 21:40:58 -0700

    SubarrayWithGivenSum

---
+++ SubArrayWithGivenSum(negative numbers).cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubarrayWithGivenSum
+{
+     class SubArrayWithGivenSum_negative_numbers_
+    {
+        public static int subArraySum(int[] arr, int n,
+                                   int sum)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int currentsum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                currentsum=currentsum+arr[i];
+                dict[currentsum] = i;
+                if(currentsum > sum)
+                {
+                    if (dict.ContainsKey(currentsum - sum))
+                    {
+                        Console.WriteLine("Sum found between" + "indexes" + dict[currentsum - sum] + " and " + i);
+                        return 1;
+                    }
+                }
+                if (currentsum == sum)
+                {
+                    Console.WriteLine("Sum found between" + "indexes" + 0 + " and " + i);
+                    return 1;
+                }
+            }
+
+            Console.WriteLine("No subarray found");
+            return 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] {10,2,-2,-20,10};
+            int n = arr.Length;
+            int sum = -10;
+
+            // Function call
+            subArraySum(arr, n, sum);
+        }
+    }
+}//Time complexity:O(n)
+ //space complexity:O(n)
---
+++ SubArrayWithGivenSum.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Cryptography.X509Certificates;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SubarrayWithGivenSum
+//{
+//    class SubArrayWithGivenSum
+//    {
+//        public static int SubArraySum(int[] a, int num)
+//        {
+//            int sum = 0, start = 0, end = 0;
+//            if (a.Length == 0)
+//                return 0;
+//            for (int i = 0; i < a.Length; i++)
+//            {
+//                if (sum < num)
+//                {
+//                    sum += a[end];
+//                    end = i + 1;
+//                }
+//                if (sum > num)
+//                {
+//                    sum -= a[start];
+//                    start++;
+
+//                }
+//                if (sum == num)
+//                {
+//                    int p = end - 1;
+//                    Console.WriteLine("Sum found between" + "indexes" + start + " and " + p);
+//                    return 1;
+//                }
+
+
+//            }
+
+//            Console.WriteLine("No subarray found");
+//            return 0;
+//        }
+//        public static void Main(string[] args)
+//        {
+//            int[] a = { 1, 4, 20, 3, 10, 5 };
+//            int num = 33;
+//            SubArraySum(a, num);
+
+//        }
+//        //time complexity:O(n)
+//        //space complexity:O(1)
+
+//    }
+//}
